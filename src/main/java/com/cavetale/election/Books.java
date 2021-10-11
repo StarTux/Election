4a55ed7314139133096415c6a9eeca65ef359d6f
StarTux <ths.loy@googlemail.com>
2021-10-11 19:58:53 +0200

    Replace deprecated references

--- src/main/java/com/cavetale/election/Books.java
+++ src/main/java/com/cavetale/election/Books.java
@@ -3,11 +3,14 @@ package com.cavetale.election;
 import com.cavetale.election.sql.SQLBallot;
 import com.cavetale.election.sql.SQLChoice;
 import com.cavetale.election.sql.SQLVote;
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.TextComponent;
+import java.util.ArrayList;
+import java.util.List;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -17,88 +20,101 @@ public final class Books {
     private Books() { }
 
     public static ItemStack makeBook(Election election, Player player) {
-        ItemStack item = new ItemStack(Material.WRITTEN_BOOK);
-        BookMeta meta = (BookMeta) item.getItemMeta();
-        meta.setTitle("Election");
-        meta.setAuthor("Cavetale");
-        meta.setGeneration(BookMeta.Generation.ORIGINAL);
-        //
-        ComponentBuilder cb = new ComponentBuilder();
-        cb.append(election.election.getName()).color(ChatColor.DARK_BLUE);
+        List<Component> pages = new ArrayList<>();
+        TextComponent.Builder frontPage = Component.text();
+        frontPage.append(Component.text(election.election.getName(), NamedTextColor.DARK_BLUE));
         if (election.election.getDescription() != null) {
-            cb.append("\n\n").reset();
-            cb.append(election.election.getDescription());
+            frontPage.append(Component.newline());
+            frontPage.append(Component.newline());
+            frontPage.append(Component.text(election.election.getDescription()));
         }
-        meta.spigot().addPage(cb.create());
+        pages.add(frontPage.build());
         for (SQLChoice choice : election.choices) {
-            cb = new ComponentBuilder();
-            cb.append(choice.getName()).color(ChatColor.DARK_BLUE);
-            if (choice.getDescription() != null) {
-                cb.append("\n").reset();
-                cb.append(choice.getDescription());
-            }
-            if (choice.getUrl() != null) {
-                cb.append("\n\n").reset();
-                cb.append("Link: ").color(ChatColor.BLACK);
-                cb.append("Click here").color(ChatColor.DARK_BLUE).underlined(true)
-                    .event(new ClickEvent(ClickEvent.Action.OPEN_URL, choice.getUrl()));
-            }
-            if (choice.getWarpJson() != null) {
-                cb.append("\n\n").reset();
-                cb.append("Warp: ").color(ChatColor.BLACK);
-                String cmd =  "/elect " + election.election.getName() + " warp " + choice.getName();
-                cb.append("[Click Here]").color(ChatColor.DARK_GREEN)
-                    .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, cmd));
-            }
-            cb.append("\n\n").reset();
-            switch (election.election.getType()) {
-            case PICK_ONE: {
-                SQLBallot ballot = election.findBallot(player.getUniqueId());
-                boolean elected = ballot != null && ballot.getChoiceId() == choice.getId();
-                if (elected) {
-                    cb.append("\u2612 Voted").color(ChatColor.GOLD).bold(true);
-                } else {
-                    cb.append("\u2610 [Vote]").color(ChatColor.DARK_BLUE);
-                    cb.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                                            TextComponent.fromLegacyText(ChatColor.GOLD + "Click here to vote")));
-                    String cmd =  "/elect " + election.election.getName() + " vote " + choice.getName();
-                    cb.event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, cmd));
-                }
-                break;
-            }
-            case UP_DOWN_VOTE: {
-                SQLVote vote = election.findVote(player.getUniqueId(), choice);
-                int value = vote != null ? vote.getValue() : 0;
-                cb.append("[Upvote]");
-                if (value == 1) {
-                    cb.color(ChatColor.GOLD).bold(true);
-                    String cmd =  "/elect " + election.election.getName() + " none " + choice.getName();
-                    cb.event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, cmd));
-                } else {
-                    cb.color(ChatColor.DARK_GREEN);
-                    String cmd =  "/elect " + election.election.getName() + " up " + choice.getName();
-                    cb.event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, cmd));
-                }
-                cb.append(" ").reset();
-                cb.append("[Downvote]");
-                if (value == -1) {
-                    cb.color(ChatColor.GOLD).bold(true);
-                    String cmd =  "/elect " + election.election.getName() + " none " + choice.getName();
-                    cb.event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, cmd));
-                } else {
-                    cb.color(ChatColor.DARK_GREEN);
-                    String cmd =  "/elect " + election.election.getName() + " down " + choice.getName();
-                    cb.event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, cmd));
-                }
-                break;
-            }
-            default:
-                break;
-            }
-            meta.spigot().addPage(cb.create());
+            pages.add(makeChoicePage(election, choice, player));
         }
-        //
-        item.setItemMeta(meta);
+        ItemStack item = new ItemStack(Material.WRITTEN_BOOK);
+        item.editMeta(m -> {
+                BookMeta meta = (BookMeta) m;
+                meta.setTitle("Election");
+                meta.setAuthor("Cavetale");
+                meta.setGeneration(BookMeta.Generation.ORIGINAL);
+                meta.pages(pages);
+            });
         return item;
+    }
+
+    public static Component makeChoicePage(Election election, SQLChoice choice, Player player) {
+        TextComponent.Builder cb = Component.text();
+        cb.append(Component.text(choice.getName(), NamedTextColor.DARK_BLUE));
+        if (choice.getDescription() != null) {
+            cb.append(Component.newline());
+            cb.append(Component.text(choice.getDescription()));
+        }
+        if (choice.getUrl() != null) {
+            cb.append(Component.newline());
+            cb.append(Component.newline());
+            cb.append(Component.text("Link: "));
+            cb.append(Component.text("Click here", NamedTextColor.DARK_BLUE, TextDecoration.UNDERLINED)
+                      .clickEvent(ClickEvent.openUrl(choice.getUrl()))
+                      .hoverEvent(HoverEvent.showText(Component.text(choice.getUrl(), NamedTextColor.BLUE))));
+        }
+        if (choice.getWarpJson() != null) {
+            cb.append(Component.newline());
+            cb.append(Component.newline());
+            cb.append(Component.text("Warp: "));
+            String cmd =  "/elect " + election.election.getName() + " warp " + choice.getName();
+            cb.append(Component.text("[Click Here]", NamedTextColor.DARK_GREEN)
+                      .clickEvent(ClickEvent.runCommand(cmd))
+                      .hoverEvent(HoverEvent.showText(Component.text("Warp to " + choice.getName()))));
+        }
+        cb.append(Component.newline());
+        cb.append(Component.newline());
+        switch (election.election.getType()) {
+        case PICK_ONE: {
+            SQLBallot ballot = election.findBallot(player.getUniqueId());
+            boolean elected = ballot != null && ballot.getChoiceId() == choice.getId();
+            if (elected) {
+                cb.append(Component.text("\u2612 Voted", NamedTextColor.GOLD, TextDecoration.BOLD));
+            } else {
+                String cmd =  "/elect " + election.election.getName() + " vote " + choice.getName();
+                cb.append(Component.text("\u2610 [Vote]", NamedTextColor.DARK_BLUE)
+                          .hoverEvent(HoverEvent.showText(Component.text("Click here to vote", NamedTextColor.GOLD)))
+                          .clickEvent(ClickEvent.runCommand(cmd)));
+            }
+            break;
+        }
+        case UP_DOWN_VOTE: {
+            SQLVote vote = election.findVote(player.getUniqueId(), choice);
+            int value = vote != null ? vote.getValue() : 0;
+            if (value == 1) {
+                String cmd = "/elect " + election.election.getName() + " none " + choice.getName();
+                cb.append(Component.text("[Upvote]", NamedTextColor.GOLD, TextDecoration.BOLD)
+                          .hoverEvent(HoverEvent.showText(Component.text("Changed your mind?", NamedTextColor.GRAY)))
+                          .clickEvent(ClickEvent.runCommand(cmd)));
+            } else {
+                cb.color(NamedTextColor.DARK_GREEN);
+                String cmd =  "/elect " + election.election.getName() + " up " + choice.getName();
+                cb.append(Component.text("[Upvote]", NamedTextColor.DARK_GREEN)
+                          .hoverEvent(HoverEvent.showText(Component.text("Click here to vote YES", NamedTextColor.GOLD)))
+                          .clickEvent(ClickEvent.runCommand(cmd)));
+            }
+            cb.append(Component.space());
+            if (value == -1) {
+                String cmd =  "/elect " + election.election.getName() + " none " + choice.getName();
+                cb.append(Component.text("[Downvote]", NamedTextColor.GOLD, TextDecoration.BOLD)
+                          .hoverEvent(HoverEvent.showText(Component.text("Changed your mind?", NamedTextColor.GRAY)))
+                          .clickEvent(ClickEvent.runCommand(cmd)));
+            } else {
+                String cmd =  "/elect " + election.election.getName() + " down " + choice.getName();
+                cb.append(Component.text("[Downvote]", NamedTextColor.DARK_GREEN)
+                          .hoverEvent(HoverEvent.showText(Component.text("Click here to vote NO", NamedTextColor.RED)))
+                          .clickEvent(ClickEvent.runCommand(cmd)));
+            }
+            break;
+        }
+        default:
+            throw new IllegalStateException("electionType=" + election.election.getType());
+        }
+        return cb.build();
     }
 }
